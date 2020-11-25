c4fc1aeefa02ca2a74387e216316f7aa44f23db6
Walid El Ahmadi <walid0896@gmail.com>
2020-11-25 21:01:51 +0100

    .

--- cart.php
+++ cart.php
@@ -1,7 +1,15 @@
 <?php include('inc/header.php'); ?>
 
 <section>
-    test
+    <h1>
+        Cart
+    </h1>
+
+    <div class="container">
+        <div class="d-flex">
+
+        </div>
+    </div>
 </section>
 
 <?php include('inc/footer.php'); ?>
